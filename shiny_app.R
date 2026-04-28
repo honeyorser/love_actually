077eaec7bc85921fcd404d7fd19df911d20f402f
Honey Orser <honey.orser@gmail.com>
2026-04-27 20:47:29 -0400

    Replaced scatter plot with a segment plot and began initial shiny app construction.

---
+++ shiny_app.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(readr)
+
+## data tidying
+
+## static visualizations
+
+## shiny app
+
+library(shiny)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      
+    ),
+    
+    mainPanel(
+      
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  
+}
+
+shinyApp(ui, server)
